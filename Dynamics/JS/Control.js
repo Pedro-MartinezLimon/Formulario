eb171f608764b3ff4c9851e0fdf15429118d0168
Pedro-MartinezLimon <Pedrolimmon@gmail.com>
2025-06-18 13:23:17 -0600

    fix: Validaciones

--- Dynamics/JS/Control.js
+++ Dynamics/JS/Control.js
@@ -2,13 +2,10 @@
 let formulario = document.getElementById('main-form');
 
 
-let usuario_nombre = document.getElementById('nombre');
-let usuario_edad = document.getElementById('edad');
-let usuario_codigo_postal = document.getElementById('CodigoPostal');
-let genero = document.querySelector("input[name='usuario-gen']:checked");
-let interes = document.querySelector("input[name='Interes']:checked");
-let pais = document.querySelector("input[name='Pais']:checked");
-    
+const usuario_nombre = document.getElementById('nombre');
+const usuario_edad = document.getElementById('edad');
+const usuario_codigo_postal = document.getElementById('CodigoPostal');
+
 let GENEROS = [ "Masculino", "Femenino", "Otro"];
 let INTERESES = ["Tecnologia", "Deportes", "Musica", "Arte"];
 let PAISES = ["Mexico", "China", "USA", "Peru", "Italia", "Francia"];
@@ -17,7 +14,29 @@ let PAISES = ["Mexico", "China", "USA", "Peru", "Italia", "Francia"];
 formulario.addEventListener("submit", function(event){
     event.preventDefault();
     
-    /**NO LOGRÉ VALIDAR :( */
+    if(isNaN(usuario_nombre.value)) console.log("Nombre valido!");
+    else console.log("Nombre invalido");
+
+    if(!isNaN(usuario_edad.value) && usuario_edad.value < 100 || usuario_edad.value > 18) console.log("Edad Valida");
+    else console.log("Edad invalida");
+
+    if(!isNaN(usuario_codigo_postal.value) || usuario_codigo_postal.value.length == 5) console.log("Código Postal valido");
+    else console.log("Invalido");
+
+
+    
+    const genero = document.querySelector("input[name='usuario-gen']:checked");
+    const interes = document.querySelector("input[name='Interes']:checked");
+    const pais = document.querySelector("input[name='Pais']:checked");
+    
+    if(GENEROS.indexOf(genero.value) != -1) console.log("Género invalido");
+    else console.log("Género valido!");
+
+    if(INTERESES.indexOf(interes.value) != -1) console.log("Intéreses validos");
+    else console.log("Intéres invalido");
+    
+    if(PAISES.indexOf(pais.value) != -1) console.log("Nación Valida");
+    else console.log("Nación invalida");
 
     console.log("Enviado");
 })
